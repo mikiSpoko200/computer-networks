c49c48e2d4bcecf097a2fdedad4212020547b132
mikiSpoko200 <328690@uwr.edu.pl>
2022-05-25 01:26:07 +0200

    Add Todo

--- transport/src/main.rs
+++ transport/src/main.rs
@@ -12,6 +12,10 @@ use std::env;
 use downloader::Downloader;
 use crate::downloader::DownloaderConfig;
 
+/* Note: Issues with recv_from, server is detected but no bytes are being sent in response.
+    maybe newline character does not match specification?
+*/
+
 fn main() {
     let config = DownloaderConfig::try_from(env::args());
     let mut downloader = Downloader::from(config);
